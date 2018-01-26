eb30f8f2e86ef4749fb79bf2e1cc4af9a6b9621a
Giulianna Mandolesi <gium.terra@gmail.com>
2018-01-26 12:36:09 -0700

    Unit 5

---
+++ Unit_5/better_hash_functions.py
@@ -0,0 +1,30 @@
+ function, hash_string,
+# that takes as inputs a keyword
+# (string) and a number of buckets,
+# and returns a number representing
+# the bucket for that keyword.
+
+def hash_string(keyword,buckets):
+    aux=0
+    for i in range (len(keyword)):
+        aux=aux + ord(keyword[i])
+    return aux%buckets
+
+
+
+
+print hash_string('a',12)
+#>>> 1
+
+print hash_string('b',12)
+#>>> 2
+
+print hash_string('a',13)
+#>>> 6
+
+print hash_string('au',12)
+#>>> 10
+
+print hash_string('udacity',12)
+#>>> 11
+
